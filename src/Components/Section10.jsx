968db77f8af70a28695929b9781fafbe01b6c37b
sindhu-121 <sindhu.g@egradtutor.in>
2024-07-29 15:16:27 +0530

    changes

--- src/Components/Section10.jsx
+++ src/Components/Section10.jsx
@@ -129,7 +129,6 @@
 
 // export default Section10;
 
-
 import React from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // Import the default styles
@@ -137,6 +136,7 @@ import Img1 from "./assets/Img1.png";
 import Img2 from "./assets/Img2.png";
 import Img3 from "./assets/Img3.png";
 import Img4 from "./assets/Img4.png";
+import Img5 from "./assets/logo-boozallenhamilton-2.svg";
 import "./Styles/Section10.css";
 
 const Section10 = () => {
@@ -151,103 +151,145 @@ const Section10 = () => {
       >
         <div className="story_slider_abt_others" key="1">
           <div id="content_div">
-            <h2>Booz Allen Hamilton Unlocks Talent Retention and Productivity Through Upskilling</h2>
-            <div>
-              <div>
-                <h4>93%</h4>
-                <p>retention rate among participating employees</p>
-              </div>
-              <div>
-                <h4>65%</h4>
-                <p>of learners noted a positive impact on their productivity
-</p>
-              </div>
+            <div className="Img5"><img src={Img5} className="content_img"  /></div>
+            
+            <div className="Booz">
+              Booz Allen Hamilton Unlocks Talent Retention and Productivity
+              Through Upskilling
             </div>
             <div>
-              <button  id="read_btn">Read full story</button>
+              <div className="participating">
+                <div>
+                <h1 className="Booz1">93%</h1>
+                <p>retention rate among participating </p><p>employees</p>
+                <hr></hr>
+                </div>
+              
+                <div>
+                <h1 className="Booz1">65%</h1>
+                <p>of learners noted a positive impact on</p> <p>their productivity</p>
+                <hr></hr>
+              </div>
+              </div>
+            
+            </div>
+            <div>
+              <button id="read_btn">Read full story <i class="fa-solid fa-arrow-right"></i></button>
             </div>
           </div>
-          <div id="img_div">
+          <div >
             <img
               src={Img1}
               alt="Img1"
-              style={{ width: "50%", height: "auto" }}
+              className="img_div"
             />
           </div>
         </div>
         <div className="story_slider_abt_others" key="2">
-          <div>
-            <h2>Capital One Accelerates Transformational Learning through Udemy Business</h2>
-            <div>
-              <div>
-                <h4>95%</h4>
-                <p>of learners rated Udemy as “very helpful” to their success</p>
-              </div>
-              <div>
-                <h4>65%</h4>
-                <p>increase in retention for in-demand tech roles</p>
-              </div>
+        <div id="content_div">
+            <div className="Img5"><img src={Img5} className="content_img"  /></div>
+            
+            <div className="Booz">
+            Capital One Accelerates Transformational Learning through Udemy
+            Business
             </div>
             <div>
-              <button  id="read_btn">Read full story</button>
+              <div className="participating">
+                <div>
+                <h1 className="Booz1">95%</h1>
+                <p> of learners rated Udemy as “very </p><p>helpful” to their success</p>
+                <hr></hr>
+                </div>
+              
+                <div>
+                <h1 className="Booz1">65%</h1>
+                <p>increase in retention for in-demand</p> <p>tech roles</p>
+                <hr></hr>
+              </div>
+              </div>
+            
+            </div>
+            <div>
+              <button id="read_btn">Read full story <i class="fa-solid fa-arrow-right"></i></button>
             </div>
           </div>
-          <div>
+          <div >
             <img
-              src={Img2}
+             src={Img2}
               alt="Img2"
-              style={{ width: "50%", height: "auto" }}
+              className="img_div"
             />
           </div>
         </div>
         <div className="story_slider_abt_others" key="3">
-          <div>
-            <h2>Eventbrite Navigates Change Through Skill-Building and Leadership Development</h2>
-            <div>
-              <div>
-                <h4>4,800+</h4>
-                <p>increase in employee enrollments for professional development courses</p>
-              </div>
-              <div>
-                <h4>65%</h4>
-                <p>revenue growth supported by a business model backed by learning</p>
-              </div>
+        <div id="content_div">
+            <div className="Img5"><img src={Img5} className="content_img"  /></div>
+            <div className="Booz">
+            Eventbrite Navigates Change Through Skill-Building and Leadership
+            Development
             </div>
             <div>
-              <button  id="read_btn">Read full story</button>
+              <div className="participating">
+                <div>
+                <h1 className="Booz1">4,800+</h1>
+                <p> increase in employee enrollments for </p><p>professional development  courses</p>
+                <hr></hr>
+                </div>
+              
+                <div>
+                <h1 className="Booz1">65%</h1>
+                <p>revenue growth supported by a business </p> <p>model backed by learning</p>
+                <hr></hr>
+              </div>
+              </div>
+            
+            </div>
+            <div>
+              <button id="read_btn">Read full story <i class="fa-solid fa-arrow-right"></i></button>
             </div>
           </div>
-          <div>
+          <div >
             <img
-              src={Img3}
+             src={Img3}
               alt="Img3"
-              style={{ width: "50%", height: "auto" }}
+              className="img_div"
             />
           </div>
         </div>
         <div className="story_slider_abt_others" key="4">
-          <div>
-            <h2>Toyota Tsusho Enhances its L&D Program to Improve Employee Outcomes</h2>
-            <div>
-              <div>
-                <h4>50%</h4>
-                <p>training cost reduction per person</p>
-              </div>
-              <div>
-                <h4>+7000</h4>
-                <p>hours of upskilling</p>
-              </div>
+        <div id="content_div">
+            <div className="Img5"><img src={Img5} className="content_img"  /></div>
+            <div className="Booz">
+            Toyota Tsusho Enhances its L&D Program to Improve Employee
+              Outcomes
             </div>
             <div>
-              <button id="read_btn">Read full story</button>
+              <div className="participating">
+                <div>
+                <h1 className="Booz1">50%</h1>
+                <p>training cost reduction per person</p>
+                <hr></hr>
+                </div>
+              
+                <div>
+                <h1 className="Booz1">+7000</h1>
+                <p>hours of upskilling</p>
+                <hr></hr>
+              </div>
+              </div>
+            
+            </div>
+            <div>
+              <button id="read_btn">Read full story <i class="fa-solid fa-arrow-right"></i></button>
             </div>
           </div>
+         
           <div>
             <img
               src={Img4}
               alt="Img4"
               // style={{ width: "50%", height: "auto" }}
-              id="slide_img"
+              className="img_div"
             />
           </div>
         </div>
